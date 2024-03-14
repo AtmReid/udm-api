3818430311e34b15e10acd3669214d18d5de8155
ArtemReid <dartisfan90@gmial.com>
2024-03-15 03:15:01 +0500

    initial commit

---
+++ src/main/java/com/example/udmapi/UdmApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.udmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/udmapi/UdmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
